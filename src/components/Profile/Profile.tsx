2d0b1aed8ce41a8bd476811cf160356e156db3c8
KDhanak <kishandhanak306@gmail.com>
2025-01-02 20:46:17 +1100

    Resume field added.

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { FaGripLines } from 'react-icons/fa';
+import { FaGripLines, FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 interface ProfileProps {
     onExperienceClick: () => void;
@@ -9,7 +9,7 @@ interface ProfileProps {
     onAboutClick: () => void;
 }
 
-const Profile: React.FC<ProfileProps> = ({onExperienceClick, onProjectClick, onAboutClick}) => {
+const Profile: React.FC<ProfileProps> = ({ onExperienceClick, onProjectClick, onAboutClick }) => {
 
     const [selected, setSelected] = useState<string>('About');
 
@@ -28,7 +28,7 @@ const Profile: React.FC<ProfileProps> = ({onExperienceClick, onProjectClick, onA
         }
     }
 
-    return(
+    return (
         <div className="container">
             <p className="text-5xl font-semibold text-primary pb-4">Kishan Dhanak</p>
             <p className="text-xl font-medium text-secondary pb-4">Junior Full-Stack Engineer</p>
@@ -39,33 +39,39 @@ const Profile: React.FC<ProfileProps> = ({onExperienceClick, onProjectClick, onA
             <div className="menu mt-20 w-fit flex flex-col gap-4">
                 {menuItems.map((item) => (
                     <div
-                    key={item}
-                    className={`flex items-center gap-2 cursor-pointer group ${
-                        selected === item ? 'text-tertiary' : ''
-                    }`}
-                    onClick={() => handleMenuClick(item)}
+                        key={item}
+                        className={`flex items-center gap-2 cursor-pointer group ${selected === item ? 'text-tertiary' : ''
+                            }`}
+                        onClick={() => handleMenuClick(item)}
                     >
                         <FaGripLines
-                        className={`transition-all duration-300 group-hover:w-10 ${
-                            selected === item ? 'w-10 text-tertiary' : ''
-                        }`}
+                            className={`transition-all duration-300 group-hover:w-10 ${selected === item ? 'w-10 text-tertiary' : ''
+                                }`}
                         />
                         <p
-                        className={`group-hover:text-tertiary transition-all duration-300 ${
-                            selected === item ? 'text-accent-light' : ''
-                        }`}
+                            className={`group-hover:text-tertiary transition-all duration-300 ${selected === item ? 'text-accent-light' : ''
+                                }`}
                         >{item}</p>
-                        </div>
+                    </div>
                 ))}
             </div>
 
-            <div className='social-media flex fixed gap-4 lg:bottom-20 lg:top-auto lg:right-auto top-8 right-16'>
-                <a href='https://github.com/KDhanak' target='_blank' rel='noopener noreferrer'>
-                    <FaGithub className='lg:size-7 cursor-pointer size-5' />
-                </a>
-                <a href='https://www.linkedin.com/in/kishandhanak306/' target='_blank' rel='noopener noreferrer'>
-                    <FaLinkedin className='lg:size-7 cursor-pointer size-5' />
-                </a>
+
+            <div className='flex fixed flex-col lg:bottom-32 lg:top-auto lg:right-auto top-10 right-16'>
+                <div className='group'>
+                    <a className='flex cursor-pointer text-secondary transition-all duration-300 font-normal group-hover:text-accent-tertiary_light' href='https://storage.googleapis.com/portfolio-v2-static-data/Resume.docx'>
+                        <p className='font-medium'>My Resume</p>
+                        <HiDownload className='mt-1 ml-1' />
+                    </a>
+                </div>
+                <div className='social-media flex fixed gap-4 lg:bottom-20 lg:top-auto lg:right-auto top-20 right-16'>
+                    <a href='https://github.com/KDhanak' target='_blank' rel='noopener noreferrer'>
+                        <FaGithub className='lg:size-7 cursor-pointer size-5' />
+                    </a>
+                    <a href='https://www.linkedin.com/in/kishandhanak306/' target='_blank' rel='noopener noreferrer'>
+                        <FaLinkedin className='lg:size-7 cursor-pointer size-5' />
+                    </a>
+                </div>
             </div>
         </div>
     )
