ae81633ca55e5d09160b3d9bf27400104a161060
KDhanak <kishandhanak306@gmail.com>
2026-05-18 18:40:35 +1000

    order of the dates changed

--- src/components/Details/Experience.tsx
+++ src/components/Details/Experience.tsx
@@ -9,7 +9,10 @@ const Experience: React.FC = () => {
     const sortedItems = experiencesData.sort((a,b) => {
         if (a.isPresent && !b.isPresent) return -1;
         if (!a.isPresent && b.isPresent) return 1;
-        return 0;
+        if (a.isPresent && b.isPresent) {
+            return new Date(b.startDate).getTime() - new Date(a.startDate).getTime();
+        }
+        return new Date(b.endDate).getTime() - new Date(a.endDate).getTime();
     })
 
     if (loading) return <p>Loading...</p>
